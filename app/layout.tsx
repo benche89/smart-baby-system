7ee5396d4217cf054dd6d3ffea23fc5a85414f43
Georgiana <gaby_rapid2006@yahoo.com>
2026-04-12 13:09:47 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Smart Baby System",
@@ -15,6 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+
+        {/* 🔥 VERCEL ANALYTICS */}
+        <Analytics />
 
         <footer
           style={{
